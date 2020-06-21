ab10dd10f1e685970eeb02cd961f3289e4e39e65
gaoyashuai <18939206470@163.com>
2020-06-21 21:46:06 +0800

    commit ParentParam

---
+++ src/main/java/com/dome/param/ParentParam.java
@@ -0,0 +1,13 @@
+package com.dome.param;
+
+/**
+ * CLASSNAME ParentParam
+ *
+ * @author gaoyashuai
+ * @description:
+ * @date 2020/6/21 21:44
+ * @Version 1.0
+ */
+public class ParentParam {
+    private String param01;
+}
